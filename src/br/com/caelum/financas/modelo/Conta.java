8abff2c19d8bafc0c1b373666f3d7387afbafa17
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-18 10:34:11 -0200

    3.12 Adicionada classe Conta

---
+++ src/br/com/caelum/financas/modelo/Conta.java
@@ -0,0 +1,52 @@
+package br.com.caelum.financas.modelo;
+
+public class Conta {
+	
+	private String titular;
+	private String agencia;
+	private String numero;
+	private String banco;
+	
+	private Integer id;
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
