4e5e8487040d20b8a13f34bbaeda8b26a1043f08
Wilson Farias <wilsonsf1@gmail.com>
2017-12-06 02:18:34 -0300

    AspectJML: anotada classe Movimentacao

--- src/br/com/caelum/financas/modelo/Movimentacao.java
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -24,6 +24,7 @@ public class Movimentacao {
 	
 	//@ public invariant valor >= 0;
 	//@ public invariant conta != null;
+	//@ public invariant data.owner == this;
 	
 	@GeneratedValue
 	@Id
@@ -42,60 +43,102 @@ public class Movimentacao {
 	@ManyToOne (cascade={CascadeType.PERSIST})
 	private /*@ spec_public @*/  Conta conta;
 	 
-	
 	/*@
-	  @ requires descricao != null;
-	  @ requires descricao.equals("");
-	  @ requires data != null;
-	  @ requires valor != null;
-	  @ requires valor.compareTo(BigDecimal.ZERO) != -1;
-	  
+	  @ requires newDescricao != null;
+	  @ requires newDescricao.equals("");
+	  @ requires newDate != null;
+	  @ requires newValor != null;
+	  @ requires newValor.compareTo(BigDecimal.ZERO) != -1;
+	  @ requires newConta != null;
+	  @ assignable descricao;
+	  @ assignable data; 
+	  @ assignable valor;
+	  @ assignable tipoMovimentacao;
+	  @ assignable conta;
 	  @ ensures id == \old(id);
+	  @ ensures descricao == newDescricao;
+	  @ ensures data == newDate;
+	  @ ensures valor == newValor;
+	  @ ensures tipoMovimentacao == tipo;
+	  @ ensures conta == newConta;
 	  @*/
-	public Movimentacao(String descricao, Calendar data, BigDecimal valor, TipoMovimentacao tipoMovimentacao,
-			Conta conta) {
-		valor.compareTo(BigDecimal.ZERO);
-		this.descricao = descricao;
-		this.data = data;
-		this.valor = valor;
-		this.tipoMovimentacao = tipoMovimentacao;
-		this.conta = conta;
+	public Movimentacao(String newDescricao, Calendar newDate, BigDecimal newValor, TipoMovimentacao tipo,
+			Conta newConta) {
+		this.descricao = newDescricao;
+		this.data = newDate;
+		this.valor = newValor;
+		this.tipoMovimentacao = tipo;
+		this.conta = newConta;
 	}
 	
+	/*
+	 * Mantendo para compatibilidade de código legado.
+	 */
+	@Deprecated
+	public Movimentacao() {}
 	
 	public /*@ pure @*/ Integer getId() {
 		return id;
 	}
-	public void setId(Integer id) {
-		this.id = id;
+	
+	/*@ requires newId != null && 0 < newId;
+	  @ assignable id;
+	  @ ensures id == newId
+	  @	&& \only_assigned(id); @*/
+	public void setId(Integer newId) {
+		this.id = newId;
 	}
 	
 	public /*@ pure @*/ String getDescricao() {
 		return descricao;
 	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	
+	/*@ requires newDescricao != null && !newDescricao.equals("");
+	  @ assignable descricao;
+	  @ ensures descricao == newDescricao 
+	  @	&& \only_assigned(descricao); @*/
+	public void setDescricao(String newDescricao) {
+		this.descricao = newDescricao;
 	}
+	
 	
 	public /*@ pure @*/ Calendar getData() {
 		return data;
 	}
-	public void setData(Calendar data) {
-		this.data = data;
+	
+	/*@ requires newData != null;
+	  @ assignable data;
+	  @ ensures data == newData 
+	  @	&& \only_assigned(descricao); @*/
+	public void setData(Calendar newData) {
+		this.data = newData;
 	}
 	
 	public /*@ pure @*/ BigDecimal getValor() {
 		return valor;
 	}
 	
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
+	/*@	public normal_behavior
+	  @		requires 0 <= valor;
+	  @		assignable valor;
+	  @		ensures valor == v;
+	  @ also
+	  @	public exceptional_behavior
+	  @		requires newValor.compareTo(BigDecimal.ZERO) != -1;
+	  @		assignable \nothing;
+	  @		ensures valor == \old(valor);
+	  @*/
+	public void setValor(BigDecimal v) {
+		this.valor = v;
 	}
 	
 	public /*@ pure @*/ Conta getConta() {
 		return conta;
 	}
 	
+	/*@	
+	  @ 
+	  @*/
 	public void setConta(Conta conta) {
 		this.conta = conta;
 	}
